bfc8291464eb6a034bd0061a3525c67a1a3ac457
Stuart Bennett <stuart.bennett@live.co.uk>
2017-08-06 17:28:30 +0100

    Remove this redundant case

--- fp-in-scala-exercises/src/2.1-recursive-fib.scala
+++ fp-in-scala-exercises/src/2.1-recursive-fib.scala
@@ -1,8 +1,8 @@
 object MyModule {
   def fib(n: Int): Int = {
+      @annotation.tailrec
       def go(n: Int, prev: Int, acc: Int): Int = n match {
         case 0 => prev
-        case 1 => acc
         case _ => go(n-1, acc, prev + acc)
       }
 
